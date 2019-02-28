fed344a8168aadc350cf8b4e5e5f01ab1b6ad07c
takanami.kodai <kohdai.takanami@gmail.com>
2019-02-28 10:37:57 +0900

    First commitgit add combination_primefactors.py README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Find Prime Factors by range
+
+Finding prime factors in between a range by python.
+
+Simple programs using for loops.
+
+The results is sent as standard output.It's showed up as lists.
+
+最大数と最低数を指定して、素因数を出力するコード。
+
+for文を使用したシンプルなコードです。
+
+結果は、標準出力されます。
+
+## Usage
+
+1. git clone
+2. just run like this! ($ python combination_primefactors.py "mini_num" "max_num")
+
---
+++ combination_primefactors.py
@@ -0,0 +1,27 @@
+
+import sys
+
+# input a base number from console.
+argvs = sys.argv
+mini_num = int(argvs[1])
+max_num  = int(argvs[2]) 
+
+
+def get_prime_numbers(mini_num,max_num):
+    
+    result = []
+    print ('The range is between %d to %d.' % (mini_num, max_num))
+
+    for num in range(mini_num,max_num + 1):
+        if num > 1:
+            for i in range(2,num):
+                if (num %i) == 0:
+                    break
+            else:
+                #print(num)
+                result.append(num)
+    return result
+
+
+prime_nums = get_prime_numbers(mini_num,max_num)
+print(prime_nums)
