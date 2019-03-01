f4a10a662e4bcfde1b08ff6f5c429c29c5383512
takanami.kodai <kohdai.takanami@gmail.com>
2019-03-01 13:44:17 +0900

    add new file(ver.adding)

--- README.md
+++ README.md
@@ -17,3 +17,24 @@ for文を使用したシンプルなコードです。
 1. git clone
 2. just run like this! ($ python combination_primefactors.py "mini_num" "max_num")
 
+
+# get combinations that can be got by adding prime numbers
+
+Finding prime factors in between a range by python3.
+
+Using priduct method, *itertools*, iterator, you can get combinations that you want to know.
+
+from the rage you set, First get prime numbers, Second print all combinations of points that can compose a given max number.
+
+最大数と最低数を指定して、素因数を取得します。
+
+イテレーター、pythonのitertoolsのproductmethodを使用して、あなたが知りたい組み合わせを求めます。
+
+あなたが指定した範囲から、まず素因数を取得します。次に、足し算をして、あなたが指定した範囲の最高値までの組み合わせを標準出力します。
+
+## Usage
+
+1. git clone
+2. just run like this! ($ python3 get_allcombinations_by_primenumbers.py "mini_num" "max_num")
+
+
---
+++ get_allcombinations_by_primenumbers.py
@@ -0,0 +1,48 @@
+
+import sys
+import pprint
+import itertools
+
+# input a base number from console.
+argvs = sys.argv
+mini_num = int(argvs[1])
+max_num  = int(argvs[2])
+
+
+# Set a range and
+# get prime numbers by for loops
+def get_prime_numbers(mini_num,max_num):
+
+    result = []
+    print ('The range is between %d to %d.' % (mini_num, max_num))
+
+    for num in range(mini_num,max_num + 1):
+        if num > 1:
+            for i in range(2,num):
+                if (num %i) == 0:
+                    break
+            else:
+                #print(num)
+                result.append(num)
+    return result
+
+
+# run
+prime_nums = get_prime_numbers(mini_num,max_num)
+print("Prime Numbers are: ", prime_nums)
+
+
+# create duplicated permutations
+def get_compositions(prime_nums):
+
+    result = []
+    for i in range(len(prime_nums), 0, -1):
+        for seq in itertools.product(prime_nums, repeat=i):
+            if sum(seq) == max_num:
+                result.append(seq)
+    return result
+
+
+pairs = get_compositions(prime_nums);
+print('All combinations are:')
+pprint.pprint(pairs)
